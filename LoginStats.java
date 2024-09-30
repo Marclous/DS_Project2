b773f74e46e84d16872c7823f8af266ccbf51f94
Marclous <47270710+Marclous@users.noreply.github.com>
2024-09-30 00:42:29 -0400

    Initial commit

---
+++ LoginStats.java
@@ -0,0 +1,15 @@
+ /**
+ * t is responsible for opening and reading the data file, obtaining user input, performing some data validation and handling all errors that may occur 
+ * (in particular, it should handle any exceptions thrown by your other classes and terminate gracefully, if need be, 
+ * with a friendly error message presented to the user: 
+ * The program should never just reprint the exception message as a way of handling an exception.
+ *
+ * @author Puyuan Song
+ */
+package project2;
+
+public class LoginStats {
+    public static void main(String[] args) {
+        
+    }
+}
